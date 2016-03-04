960938f87b6d77559d7e873d7b46a898521e32a9
BarbeBleue <simoncoteduchesne@gmail.com>
2016-03-04 17:08:40 -0500

    quelque fixes et la trame sonore

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    private int health = 100;
+    private bool alive = true;
+
+    public void characterHurt(int damage)
+    {
+        health -= damage;
+    }
+
+    public void setCharacterHealth(int patate)
+    {
+        health = patate;
+    }
+    
+	void Update () {
+
+        if (health <= 0)
+        {
+            alive = false;
+            this.gameObject.SetActive(false);
+        }
+	}
+}
